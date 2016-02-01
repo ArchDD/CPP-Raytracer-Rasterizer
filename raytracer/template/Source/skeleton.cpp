f8a27e1abb5200f4818208ba9c7b41a8ccb931ff
ArchDD <archdd@live.com>
2016-02-01 12:38:46 +0000

    defining data structures

--- raytracer/template/Source/skeleton.cpp
+++ raytracer/template/Source/skeleton.cpp
@@ -1,3 +1,4 @@
+// Have you remembered to export GLMDIR=/glm?
 #include <iostream>
 #include <glm/glm.hpp>
 #include <SDL.h>
@@ -10,22 +11,53 @@ using glm::mat3;
 
 /* ----------------------------------------------------------------------------*/
 /* GLOBAL VARIABLES                                                            */
-
+vector<Triangle> triangles;
 const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 500;
 SDL_Surface* screen;
 int t;
+
+struct Intersection
+{
+	vec3 position;
+	float distance;
+	int triangleIndex;
+};
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
 void Update();
 void Draw();
+bool ClosestIntersection(vec3 start, vec3 dir, const vector<Triangle>& triangles,
+						 Intersection& closestIntersection);
+
 
 int main( int argc, char* argv[] )
 {
 	screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
 	t = SDL_GetTicks();	// Set start value for timer.
+
+	// defines the Cornell Box
+	LoadTestModel( triangles );
+
+	// intersections
+	Triangle triangle = triangles[0]; // triangle
+	vec3 s(0,0,0); // start of ray
+	vec3 d(0,0,0); // signed distance of ray
+	// the 3D real vectors that define the triangle
+	vec3 v0 = triangle.v0;
+	vec3 v1 = triangle.v1;
+	vec3 v2 = triangle.v2;
+
+	// edges that are co-planar
+	vec3 e1 = v1 - v0;
+	vec3 e2 = v2 - v0;
+	vec3 b = s - v0;
+
+	// solve linear equation
+	mat3 A(-d, e1, e2);
+	vec3 x = glm::inverse(A) * b; // t, u, v
 
 	while( NoQuitMessageSDL() )
 	{
