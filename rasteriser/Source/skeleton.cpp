9b7d57c40498307a1f9a6ec9ff82ed52a8554563
marc_steene <ms13829@my.bristol.ac.uk>
2016-03-04 00:32:35 +0000

    Add camera rotation and translation

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 using glm::vec3;
 using glm::mat3;
+using glm::vec2;
 using glm::ivec2;
 
 /* ----------------------------------------------------------------------------*/
@@ -16,9 +17,11 @@ const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 500;
 SDL_Surface* screen;
 int t;
-float focalLength = 100.0f;
+float focalLength = 250.0f;
 
-vec3 cameraPos( 0, 0, -3.001 );
+vec3 cameraPos( 0, 0, -2.0f );
+mat3 cameraRot = mat3(0.0f);
+float yaw = 0; // Yaw angle controlling camera rotation around y-axis
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
@@ -27,6 +30,9 @@ vector<Triangle> triangles;
 void Update();
 void Draw();
 void VertexShader( const vec3& v, ivec2& p );
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result );
+void DrawLineSDL( SDL_Surface* surface, ivec2 a, ivec2 b, vec3 color );
+void DrawPolygonEdges( const vector<vec3>& vertices );
 
 int main( int argc, char* argv[] )
 {
@@ -36,10 +42,21 @@ int main( int argc, char* argv[] )
 	// Generate the Cornell Box
 	LoadTestModel( triangles );
 
+	cameraRot[1][1] = 1.0f;
+
 	while( NoQuitMessageSDL() )
 	{
 		Update();
 		Draw();
+
+		for( int y=0; y<SCREEN_HEIGHT; ++y )
+		{
+			for( int x=0; x<SCREEN_WIDTH; ++x )
+			{
+				vec3 color( 0.0, 0.0, 0.0 );
+				PutPixelSDL( screen, x, y, color );
+			}
+		}
 	}
 
 	SDL_SaveBMP( screen, "screenshot.bmp" );
@@ -53,6 +70,41 @@ void Update()
 	float dt = float(t2-t);
 	t = t2;
 	cout << "Render time: " << dt << " ms." << endl;
+
+	// Adjust camera transform
+	vec3 right(cameraRot[0][0], cameraRot[0][1], cameraRot[0][2]);
+	vec3 down(cameraRot[1][0], cameraRot[1][1], cameraRot[1][2]);
+	vec3 forward(cameraRot[2][0], cameraRot[2][1], cameraRot[2][2]);
+	Uint8* keystate = SDL_GetKeyState( 0 );
+
+	if( keystate[SDLK_UP] )
+	{
+		// Move camera forward
+		cameraPos += 0.1f*forward;
+	}
+	else if( keystate[SDLK_DOWN] )
+	{
+		// Move camera backward
+		cameraPos -= 0.1f*forward;
+	}
+	if( keystate[SDLK_LEFT] )
+	{
+		// Rotate camera to the left
+		yaw += 0.1f;
+	}
+	else if( keystate[SDLK_RIGHT] )
+	{
+		// Rotate camera to the right
+		yaw -= 0.1f;
+	}
+
+	// Update camera rotation matrix
+	float c = cos(yaw);
+	float s = sin(yaw);
+	cameraRot[0][0] = c;
+	cameraRot[0][2] = s;
+	cameraRot[2][0] = -s;
+	cameraRot[2][2] = c;
 }
 
 void Draw()
@@ -60,19 +112,14 @@ void Draw()
 	if( SDL_MUSTLOCK(screen) )
 		SDL_LockSurface(screen);
 
-	for( int i=0; i< triangles.size(); ++i )
+	for( size_t i = 0; i < triangles.size(); ++i )
 	{
 		vector<vec3> vertices(3);
 		vertices[0] = triangles[i].v0;
 		vertices[1] = triangles[i].v1;
 		vertices[2] = triangles[i].v2;
-		for(int v=0; v<3; ++v)
-		{
-			ivec2 projPos;
-			VertexShader( vertices[v], projPos );
-			vec3 color(1,1,1);
-			PutPixelSDL( screen, projPos.x, projPos.y, color );
-		}
+
+		DrawPolygonEdges( vertices );
 	}
 
 	if( SDL_MUSTLOCK(screen) )
@@ -83,6 +130,55 @@ void Draw()
 
 void VertexShader( const vec3& v, ivec2& p )
 {
-	p.x = (focalLength * (v.x / v.z)) + (SCREEN_WIDTH / 2.0f);
-	p.y = (focalLength * (v.y / v.z)) + (SCREEN_HEIGHT / 2.0f);
+	// We need to adjust the vertex positions based on the camera position
+	vec3 v2(v);
+	v2 = (v - cameraPos) * cameraRot;
+
+	p.x = (focalLength * (v2.x / v2.z)) + (SCREEN_WIDTH / 2.0f);
+	p.y = (focalLength * (v2.y / v2.z)) + (SCREEN_HEIGHT / 2.0f);
+}
+
+void DrawLineSDL( SDL_Surface* surface, ivec2 a, ivec2 b, vec3 color )
+{
+	ivec2 delta = glm::abs( a - b );
+	int pixels = glm::max( delta.x, delta.y ) + 1;
+
+	vector<ivec2> line( pixels );
+	Interpolate( a, b, line );
+
+	for(int i = 0; i < pixels; ++i)
+	{
+		vec3 color(1,1,1);
+		PutPixelSDL( surface, line[i].x, line[i].y, color );
+	}
+}
+
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result )
+{
+	int N = result.size();
+	vec2 step = vec2(b-a) / float(max(N-1,1));
+	vec2 current( a );
+	for( int i=0; i<N; ++i )
+	{
+		result[i] = current;
+		current += step;
+	}
+}
+
+void DrawPolygonEdges( const vector<vec3>& vertices )
+{
+	int V = vertices.size();
+	// Transform each vertex from 3D world position to 2D image position:
+	vector<ivec2> projectedVertices( V );
+	for( int i=0; i<V; ++i )
+	{
+		VertexShader( vertices[i], projectedVertices[i] );
+	}
+	// Loop over all vertices and draw the edge from it to the next vertex:
+	for( int i=0; i<V; ++i )
+	{
+		int j = (i+1)%V; // The next vertex
+		vec3 color( 1, 1, 1 );
+		DrawLineSDL( screen, projectedVertices[i], projectedVertices[j], color );
+	}
 }
