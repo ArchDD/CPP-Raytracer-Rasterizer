60b8987c996d1c500ed11f5e3f5209beab442c50
marc_steene <ms13829@my.bristol.ac.uk>
2016-03-04 00:06:55 +0000

    Add drawing of vertices

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 using glm::vec3;
 using glm::mat3;
+using glm::ivec2;
 
 /* ----------------------------------------------------------------------------*/
 /* GLOBAL VARIABLES                                                            */
@@ -15,17 +16,25 @@ const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 500;
 SDL_Surface* screen;
 int t;
+float focalLength = 100.0f;
+
+vec3 cameraPos( 0, 0, -3.001 );
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
+vector<Triangle> triangles;
 
 void Update();
 void Draw();
+void VertexShader( const vec3& v, ivec2& p );
 
 int main( int argc, char* argv[] )
 {
 	screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
 	t = SDL_GetTicks();	// Set start value for timer.
+
+	// Generate the Cornell Box
+	LoadTestModel( triangles );
 
 	while( NoQuitMessageSDL() )
 	{
@@ -51,12 +60,18 @@ void Draw()
 	if( SDL_MUSTLOCK(screen) )
 		SDL_LockSurface(screen);
 
-	for( int y=0; y<SCREEN_HEIGHT; ++y )
+	for( int i=0; i< triangles.size(); ++i )
 	{
-		for( int x=0; x<SCREEN_WIDTH; ++x )
+		vector<vec3> vertices(3);
+		vertices[0] = triangles[i].v0;
+		vertices[1] = triangles[i].v1;
+		vertices[2] = triangles[i].v2;
+		for(int v=0; v<3; ++v)
 		{
-			vec3 color( 1.0, 0.0, 0.0 );
-			PutPixelSDL( screen, x, y, color );
+			ivec2 projPos;
+			VertexShader( vertices[v], projPos );
+			vec3 color(1,1,1);
+			PutPixelSDL( screen, projPos.x, projPos.y, color );
 		}
 	}
 
@@ -64,4 +79,10 @@ void Draw()
 		SDL_UnlockSurface(screen);
 
 	SDL_UpdateRect( screen, 0, 0, 0, 0 );
+}
+
+void VertexShader( const vec3& v, ivec2& p )
+{
+	p.x = (focalLength * (v.x / v.z)) + (SCREEN_WIDTH / 2.0f);
+	p.y = (focalLength * (v.y / v.z)) + (SCREEN_HEIGHT / 2.0f);
 }
